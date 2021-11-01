72c316fea8a1edacab789dcb3ec2aa1f99c3f96a
DavidKorochik <david1korochik@gmail.com>
2021-11-01 17:48:28 +0200

    finished the todo app

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import Navbar from './components/Navbar.svelte';
+  import TodoList from './components/TodoList.svelte';
+  import TodoForm from './components/TodoForm.svelte';
+  import About from './components/About.svelte';
+  import Home from './components/Home.svelte';
+  import data from './store/data';
+</script>
+
+<Navbar />
+
+{#if $data.menu === 1}
+  <Home />
+{:else if $data.menu === 2}
+  <About />
+{:else}
+  <TodoForm />
+  <TodoList />
+{/if}
