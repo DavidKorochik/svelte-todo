657554ca336dcaefb11b9452884362c88ac1e461
DavidKorochik <david1korochik@gmail.com>
2021-11-01 17:45:32 +0200

    finished the home page

---
+++ src/components/Home.svelte
@@ -0,0 +1,15 @@
+<div>
+  <h1>Home Page!</h1>
+  <h2>Welcome to the Todo-App</h2>
+</div>
+
+<style>
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin-top: 100px;
+    color: teal;
+  }
+</style>
