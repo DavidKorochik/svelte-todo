eae4530ef75dc44f4ff40de5ba6322df43583dfc
DavidKorochik <david1korochik@gmail.com>
2021-11-01 17:48:02 +0200

    created a card instead of the div in TodoItem

---
+++ src/utils/TodoCard.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { fade } from 'svelte/transition';
+</script>
+
+<div transition:fade class="todo-card">
+  <slot />
+</div>
+
+<style>
+  .todo-card {
+    font-size: 20px;
+    font-weight: 500;
+  }
+</style>
