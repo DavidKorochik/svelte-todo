c48031e442e59624801de43130759eef404d6295
DavidKorochik <david1korochik@gmail.com>
2021-11-01 17:45:00 +0200

    finished the about page

---
+++ src/components/About.svelte
@@ -0,0 +1,15 @@
+<div>
+  <h1>About Page!</h1>
+  <p>Version 1.0.0</p>
+</div>
+
+<style>
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin-top: 50px;
+    color: teal;
+  }
+</style>
